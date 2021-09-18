e8c2051fc6bc47785ad8aced24cfc1a294c9621f
Bao Ho Nguyen <baohn2@vng.com.vn>
2021-09-19 00:32:17 +0700

    updated

---
+++ NETWORK SWITCH/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NETWORK_SWITCH
+{
+    public partial class Form1 : Form
+    {
+        private NetWork netwoker = new NetWork();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            switch (comboBox1.Text)
+            {
+                case "FARO VANTAGE": netwoker.SetIP("128.128.128.10", "255.255.0.0", null); MessageBox.Show("FARO VANTAGE"); break;
+                case "LEICA TRACKER": netwoker.SetIP("192.168.0.10", "255.255.255.0", null); MessageBox.Show("LEICA TRACKER"); break;
+                case "INTERNET": netwoker.Reset(); MessageBox.Show("INTERNET"); break;
+            }
+            
+        }
+    }
+}
